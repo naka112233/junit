f3f4d0c7e883b9af2996d832cb605aabf7b523de
nakamura <nakamura@gmail.com>
2022-11-14 17:24:52 +0900

    追加

--- src/test/java/junit/tutorial/ex02/e04/ConsumptionTaxTest.java
+++ src/test/java/junit/tutorial/ex02/e04/ConsumptionTaxTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 
 class ConsumptionTaxTest {
@@ -26,10 +28,46 @@ class ConsumptionTaxTest {
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-
+	
+	ConsumptionTax tax;
+	
 	@Nested
-	class test1 {
-		
+	class tax5 {
+		void taxtest() throws Exception{
+			tax = new ConsumptionTax(5);
+		}
+		@ParameterizedTest
+		@CsvSource({
+			"100, 105","3000, 3150","50, 52"
+		})
+		void test1(int price, int expected) {
+			assertEquals(expected, tax.apply(price));
+	}
+		@Nested
+		class tax10 {
+			void taxtest() throws Exception{
+				tax = new ConsumptionTax(10);
+			}
+			@ParameterizedTest
+			@CsvSource({
+				"50, 55",
+			})
+			void test1(int price, int expected) {
+				assertEquals(expected, tax.apply(price));
+		}
+	}
+		@Nested
+		class tax3 {
+			void taxtest() throws Exception{
+				tax = new ConsumptionTax(3);
+			}
+			@ParameterizedTest
+			@CsvSource({
+				"50, 51",
+			})
+			void test1(int price, int expected) {
+				assertEquals(expected, tax.apply(price));
+		}
 	}
 
 }
